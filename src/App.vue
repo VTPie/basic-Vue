48cbc71ca2e1143c26f942afa5141365d2aa7935
Vo Phong <phongwind21092001@gmail.com>
2023-10-30 15:09:53 +0700

    feat(ad): dynamic components

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <script setup>
   import { ref } from 'vue';
   import Child from './components/Child.vue';
-  
+  import OtherChild from './components/OtherChild.vue'
+  import OtherOtherChild from './components/OtherOtherChild.vue'
+
   const postFontSize = ref(1)
+  const currentTab = ref('OtherChild')
+  const tabs = {
+    OtherChild,
+    OtherOtherChild
+  }
 </script>
 
 <template>
@@ -11,6 +18,16 @@
     <Child name="Phong" @enlargeText="postFontSize += 0.1">
       This is content distribution with slots
     </Child>
+  </div>
+  <div class="demo">
+    <button
+       v-for="(_, tab) in tabs"
+       :key="tab"
+       @click="currentTab = tab"
+     >
+      {{ tab }}
+    </button>
+	  <component :is="tabs[currentTab]"></component>
   </div>
 </template>
 
---
+++ src/components/OtherOtherChild.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>This is Other-other-child component</h1>
+</template>
