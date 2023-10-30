19d69259944c3d1275869017c228b0473d8683e2
Vo Phong <phongwind21092001@gmail.com>
2023-10-30 14:41:23 +0700

    create component and passing props

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
-
+  import Child from './components/Child.vue';
 </script>
 
 <template>
   <h1>This is my basic-vue project</h1>
+  <Child name="Phong"></Child>
 </template>
 
 <style>
---
+++ src/components/Child.vue
@@ -0,0 +1,7 @@
+<script setup>
+    defineProps(['name'])
+</script>
+
+<template>
+  <h1>My name is {{ name }}</h1>
+</template>
