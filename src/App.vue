5d029d2fa9f9518abe915d9e7a16587e6b06c8fe
Vo Phong <phongwind21092001@gmail.com>
2023-10-30 14:45:12 +0700

    feat(ad): listening to events from child component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup>
+  import { ref } from 'vue';
   import Child from './components/Child.vue';
+  
+  const postFontSize = ref(1)
 </script>
 
 <template>
   <h1>This is my basic-vue project</h1>
-  <Child name="Phong"></Child>
+  <div :style="{ fontSize: postFontSize + 'em' }">
+    <Child name="Phong" @enlargeText="postFontSize += 0.1">
+      This is content distribution with slots
+    </Child>
+  </div>
 </template>
 
 <style>
--- src/components/Child.vue
+++ src/components/Child.vue
@@ -1,7 +1,9 @@
 <script setup>
     defineProps(['name'])
+    defineEmits(['enlargeText'])
 </script>
 
 <template>
   <h1>My name is {{ name }}</h1>
+  <button @click="$emit('enlargeText')">Enlarge text</button>
 </template>
